60fd8bff228e9cb3544a0e59d6562e821bc75890
Sir Alter <Sir Alter>
2016-10-29 17:32:39 +0300

    Week 4 final assignment completed

---
+++ Yandex/Week 4/fondIndexComposition.py
@@ -0,0 +1,85 @@
+from sklearn.decomposition import PCA
+from principalComponentAnalysis import DataLoader
+
+import numpy as np
+import pandas as pd
+
+URL_PRICES = 'https://raw.githubusercontent.com/EvilAvenger/Machine-Learning/master/Yandex/Week%204/data/close_prices.csv'
+URL_INDEX = 'https://raw.githubusercontent.com/EvilAvenger/Machine-Learning/master/Yandex/Week%204/data/djia_index.csv'
+FOLDER_NAME = 'data'
+
+def __main__():
+    
+    data = get_data(URL_PRICES, FOLDER_NAME)
+    (pca, result) = train_pca(get_matrix(data))
+
+    num = get_first_indexies(pca.explained_variance_ratio_)
+    print(num)
+    
+    dj_data  = get_matrix(get_data(URL_INDEX, FOLDER_NAME))
+    correlation = count_pearson_corr(result[0:,0], dj_data)
+    print(correlation)
+
+    index = get_company_name(pca.components_)
+    print(data.columns.values[index + 1])
+    
+
+def get_data(url, folder):
+    loader = DataLoader()
+    data = loader.download_data(url, folder)
+    return data
+
+def get_matrix(data):
+     return data.as_matrix()[0:,1:]
+
+def train_pca(data):
+    pca = PCA(n_components=10)
+    result = pca.fit_transform(data)
+    return (pca, result)
+
+def get_first_indexies(indexies):
+    sum = i = 0
+    while sum <= 0.90:
+        sum = sum + indexies[i]
+        i = i + 1
+    return i
+
+def get_company_name(values):
+    values = values[0].tolist()
+    return values.index(max(values))
+
+def count_pearson_corr(data, djdata):
+    djdata = list(np.reshape(djdata, len(djdata)))
+    result = np.corrcoef(data, djdata)
+    return round(result[0][1], 2)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    __main__()
--- Yandex/Week 4/principalComponentAnalysis.py
+++ Yandex/Week 4/principalComponentAnalysis.py
@@ -62,20 +62,20 @@ class  DataLoader(object):
     def __init__(self):
         pass
         
-    def __validate_data(self, url_name):
+    def _validate_data(self, url_name):
         result = os.path.exists(url_name)
         return result
 
-    def __get_folder_path(self, url, folder_name):
+    def _get_folder_path(self, url, folder_name):
         dir_path = os.path.dirname(os.path.realpath(__file__))
         file_name = url.rsplit('/')[-1]
         data_path = os.path.join(dir_path, folder_name, file_name)
         return data_path
 
     def download_data(self, url_name, folder_name):
-        path = self.__get_folder_path(url_name, folder_name)
+        path = self._get_folder_path(url_name, folder_name)
 
-        if not self.__validate_data(path):
+        if not self._validate_data(path):
             urllib.request.urlretrieve(url_name, path)
         return pd.read_csv(path)
 
